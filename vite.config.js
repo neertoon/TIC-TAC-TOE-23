cb2673cd061882eacdc96b8f8e9dd0671ce9c166
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-12-31 19:48:14 +0100

    Docker - uruchomienie na porcie 5173 z hot reload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      clientPort: 5173,
+    },
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [react()],
 })
