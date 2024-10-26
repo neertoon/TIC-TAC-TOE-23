1f97788a44b09c3e76bb9d70c7f7ad20dd9122a2
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2024-10-26 10:54:37 +0200

    zmniejszenie liczby useState przez ustalanie aktywnego playera na podstawie listingu ruchów na planszy

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import Player from "./components/Player.jsx";
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = 'X';
+
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+
+    return currentPlayer;
+}
+
 function App() {
   const [ gameTurns, setGameTurns ] = useState([]);
-  const [ activePlayer, setActivePlayer ] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex) {
-      setActivePlayer((currentlyActivePlayer) => currentlyActivePlayer === 'X' ? 'O' : 'X');
       setGameTurns(prevTurns => {
-          let currentPlayer = 'X';
-
-          if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-              currentPlayer = 'O';
-          }
+          let currentPlayer = deriveActivePlayer(prevTurns);
 
           const updatedTurns = [{ player: currentPlayer, square: {row: rowIndex, col: colIndex } }, ...prevTurns];
 
